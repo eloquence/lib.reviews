ecc41a274f0eb67ac47d0d38b1f4c31f743a8a14
Erik Moeller <eloquence@gmail.com>
2016-06-04 02:04:48 -0700

    Only offer languages not currently selected.
    Fixes #1, for now.

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
+// locale names and message keys
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'lib.reviews' });
+  let languages = { 'de': 'german', 'en': 'english' };
+  // We don't offer the active language
+  delete languages[req.locale];
+  res.render('index', { languages });
 });
 
 module.exports = router;
