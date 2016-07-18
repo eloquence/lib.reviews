8d0db6e4708a36b22591ecf2ec0d5de031a5d494
Erik Moeller <eloquence@gmail.com>
2016-07-18 13:33:47 -0700

    Move more stuff into config.
    Fixes #8.

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 'use strict';
 const Thinky = require('thinky');
+const config = require('config');
+const debug = require('./util/debug');
 const thinky = Thinky({
-  host: 'localhost',
-  port: 28015,
-  db: 'libreviews'
-});
+    servers: config.dbServers,
+    db: config.dbName,
+    user: config.dbUser,
+    password: config.dbPassword,
+    silent: true
+  });
+thinky.r.getPoolMaster().on('log', debug.db);
+
 module.exports = thinky;
