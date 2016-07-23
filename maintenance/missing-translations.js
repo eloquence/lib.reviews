c8de557ce71c6de9a85051fa676f0000ed52c5bd
Erik Moeller <eloquence@gmail.com>
2016-07-22 19:44:17 -0700

    Throw in extra keys for good measure.

--- maintenance/missing-translations.js
+++ maintenance/missing-translations.js
@@ -9,15 +9,21 @@ for (let langKey of langKeys) {
     continue;
 
   let messageKeys = Object.keys(jsonfile.readFileSync(`../locales/${langKey}.json`));
-  let missingKeys = enMessageKeys.filter(getMissingKeys(messageKeys));
+  let missingKeys = enMessageKeys.filter(getKeyFilter(messageKeys));
 
   if (missingKeys.length) {
     console.log(`The following keys are missing from ${langKey}.json:`);
     console.log(missingKeys.join('\n'));
   }
+
+  let extraKeys = messageKeys.filter(getKeyFilter(enMessageKeys));
+  if (extraKeys.length) {
+    console.log(`\nThe following keys exist in ${langKey}.json which are not in the English version:`);
+    console.log(extraKeys.join('\n'));
+  }
 }
 
-function getMissingKeys(xxMessageKeys) {
+function getKeyFilter(xxMessageKeys) {
   return function(ele) {
     if (xxMessageKeys.indexOf(ele) == -1)
       return true;
