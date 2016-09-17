bddda3a057184c240c8f7fbd63ae16b1b7d02d6c
Erik Moeller <eloquence@gmail.com>
2016-09-17 15:58:52 -0700

    UI tweak: show different icon for multiple files

--- frontend/upload.js
+++ frontend/upload.js
@@ -11,6 +11,11 @@
       $('#upload-label').text(originalLabel);
     } else {
       let countLabel = count == 1 ? config.messages['one file selected'] : config.messages['files selected'];
+      // We use a different icon to represent multiple files
+      if (count == 1)
+        $('#upload-icon').removeClass('fa-files-o').addClass('fa-file-image-o');
+      else
+        $('#upload-icon').removeClass('fa-file-image-o').addClass('fa-files-o');
       countLabel = countLabel.replace('%s', String(count));
       $('#upload-label-text').text(countLabel);
       $('#start-upload').prop('disabled', false);
