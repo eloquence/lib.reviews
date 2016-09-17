7faf0c6f6993ca5bbda22b8d41525b580ade27f4
Erik Moeller <eloquence@gmail.com>
2016-09-16 17:39:50 -0700

    Perform content validation on file upload
    We do shallow magic number checks + SVG validation for now
    
    Fixes #58

--- routes/process-uploads.js
+++ routes/process-uploads.js
@@ -5,13 +5,18 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const checkCSRF = require('csurf')();
+const fileType = require('file-type');
+const readChunk = require('read-chunk');
+const fs = require('fs');
+const isSVG = require('is-svg');
 
 // Internal dependencies
 const Thing = require('../models/thing');
 const getResourceErrorHandler = require('./handlers/resource-error-handler');
 const render = require('./helpers/render');
+const debug = require('../util/debug');
 
-const allowedTypes = ['image/png', 'image/gif', 'image/svg', 'image/jpeg', 'video/webm', 'audio/ogg', 'video/ogg', 'audio/mpeg'];
+const allowedTypes = ['image/png', 'image/gif', 'image/svg+xml', 'image/jpeg', 'video/webm', 'audio/ogg', 'video/ogg', 'audio/mpeg'];
 
 router.post('/thing/:id/upload', function(req, res, next) {
 
@@ -51,17 +56,36 @@ router.post('/thing/:id/upload', function(req, res, next) {
         if (error)
           return next(error);
 
-        // TODO: We need to inspect files to ensure they are the claimed MIME
-        // type.
-
         if (req.files.length) {
           req.files.forEach(f => thing.addFile(f.filename));
 
-          thing
-            .save()
+          let validators = [];
+          req.files.forEach(file => {
+            // SVG files need full examination
+            if (file.mimetype != 'image/svg+xml')
+              validators.push(validateFile(file.path, file.mimetype));
+            else
+              validators.push(validateSVG(file.path));
+          });
+
+
+          // Validate all files
+          Promise
+            .all(validators)
             .then(() => {
-              req.flash('pageMessages', req.__('upload completed'));
+              thing
+                .save()
+                .then(() => {
+                  req.flash('pageMessages', req.__('upload completed'));
+                  res.redirect(`/thing/${thing.id}`);
+                });
+            })
+            .catch(_error => { // One of the files couldn't be validated
+
+              cleanupFiles(req);
+              req.flash('pageErrors', req.__('upload failed'));
               res.redirect(`/thing/${thing.id}`);
+
             });
 
         } else {
@@ -94,5 +118,60 @@ router.post('/thing/:id/upload', function(req, res, next) {
     .catch(getResourceErrorHandler(req, res, next, 'thing', id));
 
 });
+
+function cleanupFiles(req) {
+  req.files.forEach(file => {
+    fs.unlink(file.path, error => {
+      if (error)
+        debug.error({
+          context: 'upload',
+          error,
+          req
+        });
+    });
+  });
+}
+
+
+// Verify that a file's contents match its claimed MIME type. This is shallow,
+// fast validation. If files are manipulated, we need to pay further attention
+// to any possible exploits.
+function validateFile(path, claimedType) {
+
+  return new Promise((resolve, reject) => {
+
+    readChunk(path, 0, 262)
+      .then(buffer => {
+        let type = fileType(buffer);
+        if (!type)
+          return reject(new Error('Unrecognized file type'));
+        if (type.mime === claimedType)
+          return resolve();
+        if (type.mime !== claimedType)
+          return reject(new Error(`Claimed MIME type was ${claimedType} but file appears to be ${type.mime}`));
+      })
+      .catch(error => reject(error));
+  });
+
+}
+
+// SVGs can't be validated by magic number check. This, too, is a relatively
+// shallow validation, not a full XML parse.
+function validateSVG(path) {
+
+  return new Promise((resolve, reject) => {
+
+    fs.readFile(path, (error, data) => {
+      if (error)
+        return reject(error);
+
+      if (isSVG(data))
+        return resolve();
+      else
+        return reject(new Error(`Claimed MIME type was image/svg+xml but file does not appear to be valid SVG.`));
+    });
+  });
+
+}
 
 module.exports = router;
