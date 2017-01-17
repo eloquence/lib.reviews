1654d296e5618a71db10558fe3b98e2827088037
Erik Moeller <eloquence@gmail.com>
2017-01-16 22:29:44 -0800

    Activate Hungarian

--- locales/languages.js
+++ locales/languages.js
@@ -11,7 +11,7 @@ const path = require('path');
 // with the translations to the locales/ directory. Then add the new language
 // code to this array. Language names will be automatically imported from CLDR
 // on the next restart.
-const validLanguages = ['en', 'bn', 'de', 'eo', 'es', 'fr', 'it', 'mk', 'nl', 'pt', 'pt-PT', 'sv', 'zh', 'zh-Hant'];
+const validLanguages = ['en', 'bn', 'de', 'eo', 'es', 'fr', 'hu', 'it', 'mk', 'nl', 'pt', 'pt-PT', 'sv', 'zh', 'zh-Hant'];
 
 const languageNameMap = {
   // CLDR uses the unqualified key (e.g., "pt" for Portuguese) for the version
