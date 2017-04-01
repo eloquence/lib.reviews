65fa30218bb90f4559d3f9fdfc02038dbf443771
Erik Moeller <eloquence@gmail.com>
2017-03-31 19:37:29 -0700

    Automatically disambiguate reserved slugs

--- models/thing-slug.js
+++ models/thing-slug.js
@@ -3,6 +3,9 @@ const thinky = require('../db');
 const r = thinky.r;
 const type = thinky.type;
 
+// You can use these slugs, but they'll be automatically be qualified with a number
+const reservedSlugs = ['register', 'actions', 'signin', 'login', 'teams', 'user', 'new', 'signout', 'logout', 'api', 'faq', 'static', 'terms'];
+
 // Model for unique, human-readable identifiers ('slugs') for review subjects
 // ('things')
 
@@ -25,56 +28,64 @@ ThingSlug.ensureIndex("createdOn");
 
 ThingSlug.define("qualifiedSave", function() {
   let slug = this;
+  slug.baseName = slug.name; // Store base name for later reference
   return new Promise((resolve, reject) => {
-    slug.baseName = slug.name; // Store base name for later reference
-    this
-      .save()
-      .then(resolve)
-      .catch(error => {
-        if (error.name === 'DuplicatePrimaryKeyError') {
+    if (reservedSlugs.indexOf(slug.name.toLowerCase()) !== -1) {
+      handleConflicts();
+    } else {
+      slug
+        .save()
+        .then(resolve)
+        .catch(error => {
+          if (error.name === 'DuplicatePrimaryKeyError')
+            handleConflicts();
+          else
+            reject(error);
+        });
+    }
 
-          // Check first if we've used this base name before for this ID
-          ThingSlug
-            .filter({
-              baseName: slug.name,
-              thingID: slug.thingID
-            })
-            .orderBy(r.desc('createdOn'))
-            .limit(1)
-            .then(slugs => {
-              if (slugs.length)
-                // Got a match, no need to save -- just pass it along
-                return resolve(slugs[0]);
-              // Widen search for most recent use of this base name
-              else {
-                ThingSlug
-                  .filter({
-                    baseName: slug.name
-                  })
-                  .orderBy(r.desc('createdOn'))
-                  .limit(1)
-                  // We need to generate a new qualifier to disambiguate this slug
-                  .then(slugs => {
-                    let latestQualifier;
-                    if (slugs.length && !isNaN(Number(slugs[0].qualifierPart))) {
-                      latestQualifier = Number(slugs[0].qualifierPart) + 1;
-                    } else {
-                      latestQualifier = 2;
-                    }
-                    slug.name = slug.name + '-' + String(latestQualifier);
-                    slug.qualifierPart = String(latestQualifier);
-                    slug
-                      .save()
-                      .then(resolve)
-                      .catch(reject);
-                  })
+    function handleConflicts() {
+
+      // Check first if we've used this base name before for this ID
+      ThingSlug
+        .filter({
+          baseName: slug.name,
+          thingID: slug.thingID
+        })
+        .orderBy(r.desc('createdOn'))
+        .limit(1)
+        .then(slugs => {
+          if (slugs.length)
+            // Got a match, no need to save -- just pass it along
+            return resolve(slugs[0]);
+          // Widen search for most recent use of this base name
+          else {
+            ThingSlug
+              .filter({
+                baseName: slug.name
+              })
+              .orderBy(r.desc('createdOn'))
+              .limit(1)
+              // We need to generate a new qualifier to disambiguate this slug
+              .then(slugs => {
+                let latestQualifier;
+                if (slugs.length && !isNaN(Number(slugs[0].qualifierPart))) {
+                  latestQualifier = Number(slugs[0].qualifierPart) + 1;
+                } else {
+                  latestQualifier = 2;
+                }
+                slug.name = slug.name + '-' + String(latestQualifier);
+                slug.qualifierPart = String(latestQualifier);
+                slug
+                  .save()
+                  .then(resolve)
                   .catch(reject);
-              }
-            })
-            .catch(reject);
-        } else
-          reject(error);
-      });
+              })
+              .catch(reject);
+          }
+        })
+        .catch(reject);
+    }
 
   });
 });
