d1584dbac8a88aad9e4f206a91254f6aaa48570f
Erik Moeller <eloquence@gmail.com>
2017-07-11 15:36:57 -0700

    Fix potential redirect loop with non-encoded URIs

--- routes/helpers/slugs.js
+++ routes/helpers/slugs.js
@@ -96,8 +96,10 @@ function _resolveAndLoad(req, res, id, loadOptions, modelConfig) {
 
 // Redirect from current page to the canonical URL
 function _redirectToCanonical(req, res, id, basePath, canonicalSlugName) {
+  // Match the encoded or non-encoded version of the slug (we might get either)
+  let regex = new RegExp(`^${basePath}(${encodeURIComponent(id)}|${id})`);
   // Replace relevant substring of path so this works for any page
-  let newPath = req.path.replace(basePath + encodeURIComponent(id), basePath + encodeURIComponent(canonicalSlugName));
+  let newPath = req.path.replace(regex, basePath + encodeURIComponent(canonicalSlugName));
   res.redirect(newPath);
 }
 
