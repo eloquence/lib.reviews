1a547f64552e491f96cff46f8215e5a78e20ad92
Erik Moeller <eloquence@gmail.com>
2017-09-21 18:45:49 -0700

    Fix description display logic for logged out users
    Also fixes #155

--- util/handlebars-helpers.js
+++ util/handlebars-helpers.js
@@ -141,3 +141,26 @@ hbs.registerHelper('mlString', function(str, addLanguageSpan, options) {
 });
 
 hbs.registerHelper('round', (num, dec) => +num.toFixed(dec));
+
+hbs.registerHelper('ifCond', function(v1, operator, v2, options) {
+  switch (operator) {
+    case '==':
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case '===':
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case '<':
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case '<=':
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case '>':
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case '>=':
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case '&&':
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case '||':
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
+});
