fd654fcca5fff02c56a2cf351636c57ef30ab869
Erik Moeller <eloquence@gmail.com>
2017-11-06 21:57:24 -0800

    async/await refactor, jsdoc

--- models/blog-post.js
+++ models/blog-post.js
@@ -1,4 +1,11 @@
 'use strict';
+
+/**
+ * Model for blog posts. Blog posts can currently only be authored by teams: one
+ * team can have many blog posts.
+ *
+ * @namespace BlogPost
+ */
 const thinky = require('../db');
 const r = thinky.r;
 const type = thinky.type;
@@ -8,7 +15,8 @@ const isValidLanguage = require('../locales/languages').isValid;
 const User = require('./user');
 const TeamSlug = require('./team-slug');
 
-/* eslint-disable newline-per-chained-call */ /* for schema readability */
+/* eslint-disable newline-per-chained-call */
+/* for schema readability */
 
 let blogPostSchema = {
 
@@ -38,13 +46,133 @@ let BlogPost = thinky.createModel("blog_posts", blogPostSchema);
 BlogPost.ensureIndex("createdOn");
 BlogPost.belongsTo(User, "creator", "createdBy", "id");
 
+// NOTE: STATIC METHODS --------------------------------------------------------
+
+// Standard handlers
+
 BlogPost.createFirstRevision = revision.getFirstRevisionHandler(BlogPost);
 BlogPost.getNotStaleOrDeleted = revision.getNotStaleOrDeletedGetHandler(BlogPost);
+
+// Custom methods
+
+/**
+ * Get a blog post and the user object representing its original author
+ *
+ * @param {String} id
+ *  blog post ID
+ * @returns {BlogPost}
+ *  post with populated `.creator` property
+ * @async
+ */
+BlogPost.getWithCreator = async function(id) {
+  const post = await BlogPost
+    .get(id)
+    .getJoin({
+      creator: {
+        _apply: seq => seq.without('password')
+      }
+    });
+
+  if (post._revDeleted)
+    throw revision.deletedError;
+
+  if (post._oldRevOf)
+    throw revision.staleError;
+
+  return post;
+};
+
+/**
+ * Get the most recent blog posts for a given team
+ *
+ * @param {String} teamID
+ *  team ID to look up
+ * @param {Object} [options]
+ *  query criteria
+ * @param {Number} options.limit=10
+ *  number of posts to retrieve
+ * @param {Date} options.offsetDate
+ *  only get posts older than this date
+ * @returns {BlogPosts[]}
+ * @async
+ */
+BlogPost.getMostRecentBlogPosts = async function(teamID, {
+  limit = 10,
+  offsetDate = undefined
+} = {}) {
+
+  if (!teamID)
+    throw new Error('We require a team ID to fetch blog posts.');
+
+  let query = BlogPost;
+
+  if (offsetDate && offsetDate.valueOf)
+    query = query.between(r.minval, r.epochTime(offsetDate.valueOf() / 1000), {
+      index: 'createdOn',
+      rightBound: 'open' // Do not return previous record that exactly matches offset
+    });
+
+  query = query
+    .orderBy({ index: r.desc('createdOn') })
+    .filter({ teamID })
+    .filter({ _revDeleted: false }, { default: true })
+    .filter({ _oldRevOf: false }, { default: true })
+    .limit(limit + 1) // One over limit to check if we need potentially another set
+    .getJoin({
+      creator: {
+        _apply: seq => seq.without('password')
+      }
+    });
+
+  const posts = await query;
+  const result = {};
+
+  // At least one additional document available, set offset for pagination
+  if (posts.length == limit + 1) {
+    result.offsetDate = posts[limit - 1].createdOn;
+    posts.pop();
+  }
+
+  result.blogPosts = posts;
+  return result;
+};
+
+/**
+ * Get blog posts for a given team slug.
+ *
+ * @param {String} teamSlugName
+ *  slug (short human-readable identifier) for a team
+ * @param {Object} [options]
+ *  options supported by {@link BlogPost.getMostRecentBlogPosts}
+ * @returns {BlogPost[]}
+ * @async
+ */
+BlogPost.getMostRecentBlogPostsBySlug = async function(teamSlugName, options) {
+  const slug = await TeamSlug.get(teamSlugName);
+  const posts = BlogPost.getMostRecentBlogPosts(slug.teamID, options);
+  return posts;
+};
+
+// NOTE: INSTANCE METHODS ------------------------------------------------------
+
+// Standard handlers
 
 BlogPost.define("newRevision", revision.getNewRevisionHandler(BlogPost));
 BlogPost.define("deleteAllRevisions", revision.getDeleteAllRevisionsHandler(BlogPost));
 
-BlogPost.define("populateUserInfo", function(user) {
+// Custom methods
+
+BlogPost.define("populateUserInfo", populateUserInfo);
+
+/**
+ * Populate virtual permission fields for this post based on a given user
+ *
+ * @param {User} user
+ *  the user whose permissions to check
+ * @instance
+ * @memberof BlogPost
+ */
+function populateUserInfo(user) {
 
   if (!user) // Keep at permissions at default (false)
     return;
@@ -54,111 +182,6 @@ BlogPost.define("populateUserInfo", function(user) {
 
   if (user.isSuperUser || user.id === this.createdBy || user.isSiteModerator)
     this.userCanDelete = true;
-
-});
-
-BlogPost.getWithCreator = function(id) {
-  return new Promise((resolve, reject) => {
-    BlogPost
-      .get(id)
-      .getJoin({
-        creator: {
-          _apply: seq => seq.without('password')
-        }
-      })
-      .then(post => {
-        if (post._revDeleted)
-          return reject(revision.deletedError);
-
-        if (post._oldRevOf)
-          return reject(revision.staleError);
-
-        resolve(post);
-
-      })
-      .catch(error => reject(error));
-  });
-};
-
-BlogPost.getMostRecentBlogPosts = function(teamID, options) {
-  if (!teamID)
-    throw new Error('We require a team ID to fetch blog posts.');
-
-  options = Object.assign({ // Defaults
-    limit: 10,
-    // Only show posts older than this date. Must be JavaScript Date object.
-    offsetDate: undefined
-  }, options);
-
-  let query = BlogPost;
-
-  if (options.offsetDate && options.offsetDate.valueOf)
-    query = query.between(r.minval, r.epochTime(options.offsetDate.valueOf() / 1000), {
-      index: 'createdOn',
-      rightBound: 'open' // Do not return previous record that exactly matches offset
-    });
-
-
-  query = query.orderBy({
-      index: r.desc('createdOn')
-    })
-    .filter({
-      teamID
-    })
-    .filter({
-      _revDeleted: false
-    }, {
-      default: true
-    })
-    .filter({
-      _oldRevOf: false
-    }, {
-      default: true
-    })
-    .limit(options.limit + 1) // One over limit to check if we need potentially another set
-    .getJoin({
-      creator: {
-        _apply: seq => seq.without('password')
-      }
-    });
-
-  return new Promise((resolve, reject) => {
-
-    query
-      .then(blogPosts => {
-
-        let result = {};
-
-        // At least one additional document available, set offset for pagination
-        if (blogPosts.length == options.limit + 1) {
-          result.offsetDate = blogPosts[options.limit - 1].createdOn;
-          blogPosts.pop();
-        }
-
-        result.blogPosts = blogPosts;
-
-        resolve(result);
-      })
-      .catch(error => reject(error));
-
-  });
-
-};
-
-// Helper function to quickly get blog posts for a given slug
-BlogPost.getMostRecentBlogPostsBySlug = function(teamSlugName, options) {
-  return new Promise((resolve, reject) => {
-    TeamSlug
-      .get(teamSlugName)
-      .then(slug => {
-        BlogPost
-          .getMostRecentBlogPosts(slug.teamID, options)
-          .then(result => resolve(result))
-          .catch(error => reject(error));
-      })
-      .catch(error => reject(error)); // Slug not found?
-  });
-};
-
+}
 
 module.exports = BlogPost;
