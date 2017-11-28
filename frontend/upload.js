df6a38749ea13297defcb8b05cdbc9f73a97fe25
Erik Moeller <eloquence@gmail.com>
2017-11-27 23:58:15 -0800

    show filenames of files selected for uploading prior to upload

--- frontend/upload.js
+++ frontend/upload.js
@@ -5,20 +5,32 @@
   // We shouldn't be able to start an upload until we've selected some files.
   $('#start-upload').prop('disabled', true);
   $('#upload-input').change(() => {
-    let count = $('#upload-input')[0].files.length;
+    let files = $('#upload-input')[0].files;
+    let count = files.length;
+    let names = getNames(files);
     if (!count) {
       $('#start-upload').prop('disabled', true);
       $('#upload-label').text(originalLabel);
+      $('#file-name-container').empty();
     } else {
-      let countLabel = count == 1 ? libreviews.msg('one file selected') : libreviews.msg('files selected');
+      let countLabel = count == 1 ?
+        libreviews.msg('one file selected') :
+        libreviews.msg('files selected', { stringParam: count });
       // We use a different icon to represent multiple files
       if (count == 1)
         $('#upload-icon').removeClass('fa-files-o').addClass('fa-file-image-o');
       else
         $('#upload-icon').removeClass('fa-file-image-o').addClass('fa-files-o');
-      countLabel = countLabel.replace('%s', String(count));
       $('#upload-label-text').text(countLabel);
       $('#start-upload').prop('disabled', false);
+      $('#file-name-container').text(names.join(', '));
     }
   });
+
+  function getNames(fileList) {
+    let names = [];
+    for (let i = 0; i < fileList.length; i++)
+      names.push(fileList[i].name);
+    return names;
+  }
 }());
