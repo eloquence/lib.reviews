fc69158e6e9b4b62fa61295f01bf9dbd6abdf061
Erik Moeller <eloquence@gmail.com>
2018-02-27 23:07:38 -0800

    Add basic media archiver we can run weekly

---
+++ maintenance/archive-media.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# This script will generate an archive of all uploaded media files.
+MEDIADIR=`dirname $0`/../static/uploads
+EXPORTDIR=`dirname $0`/../static/downloads/media
+ISODATE=`date +"%Y-%m-%d"`
+EXPORTPATH="$EXPORTDIR/media-$ISODATE.tgz"
+tar cvfz $EXPORTPATH $MEDIADIR
